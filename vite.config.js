13c3637ef46785fe39558e1835031321dd65c0b4
Fadel <fadelmaulana12@gmail.com>
2024-11-07 07:06:51 +0700

    fix domain

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,14 @@ export default defineConfig(({ mode }) => {
   // Load environment variables
   const env = loadEnv(mode, process.cwd());
 
-  console.log("LOCAL_URL:", env.VITE_LOCAL_URL); // Debug, cek apakah variabel terbaca dengan benar
+
 
   return {
     plugins: [react()],
     server: {
       proxy: {
         "/api": {
-          target: env.VITE_LOCAL_URL, // Gunakan env.VITE_LOCAL_URL, bukan process.env
+          target: "https://magang-uhamka.my.id/api",
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
