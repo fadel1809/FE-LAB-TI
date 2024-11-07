d0c09cb4e68228d0e0498b63200184ebf293c817
Fadel <fadelmaulana12@gmail.com>
2024-11-07 07:08:47 +0700

    fix domainn

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 console.log(import.meta.env.DOMAIN_URL);
 const customFetch = axios.create({
-  baseURL:
-    import.meta.env.VITE_ENVIRONMENT === "production"
-      ? import.meta.env.VITE_DOMAIN_URL
-      : import.meta.env.VITE_LOCAL_URL,
+  baseURL: "https://magang-uhamka.my.id/api",
 });
 
 export default customFetch;
